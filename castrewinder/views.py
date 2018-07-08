044d1fa73478fe01f55285ab110dd285dffbce0f
Joachim <joachim.robert@gmail.com>
2018-07-08 14:18:26 +0200

    New API !

--- castrewinder/views.py
+++ castrewinder/views.py
@@ -35,22 +35,28 @@ def index():
       valid_feed = feed_worker.import_feed(request.form['url'])
       
       if not valid_feed:
-        flash("Invalid URL.")
+        flash("The supplied URL is not a podcast feed.")
 
       else:
 
         frequency = get_frequency(request_form = request.form)
         options = get_options(request_form = request.form)
 
-        feed_url = request.form['url']
-        if u.netloc == 'itunes.apple.com':
-          feed_url = feed_worker.get_feed_from_itunes_api(itunes_url = feed_url)
+        if frequency == '':
+          flash('You did not select week days, please do now or select another frequency.')
 
-        feed_object = db.session.query(Feed).filter(Feed.url == feed_url).one()
+        else:
 
-        end_url = request.url + generate_url(feed_id = feed_object.id, frequency = frequency, options = options)
+          feed_url = request.form['url']
+          if u.netloc == 'itunes.apple.com':
+            feed_url = feed_worker.get_feed_from_itunes_api(itunes_url = feed_url)
 
-        return render_template('index.html', form = form, end_url = end_url, feed_object = json.loads(feed_object.content))
+          feed_object = db.session.query(Feed).filter(Feed.url == feed_url).one()
+
+          end_url = request.host_url + generate_url(feed_id = feed_object.id,
+                                                              frequency = frequency, options = options)
+
+          return render_template('index.html', form = form, end_url = end_url, feed_object = json.loads(feed_object.content))
 
   return render_template('index.html', form = form)
 
@@ -99,3 +105,68 @@ def serve_feed(feed_id, frequency, start_date, options):
 def about():
   return render_template('about.html')
 
+@app.route('/api/get', methods=["GET"])
+@app.route('/api/post', methods=["POST"])
+def api():
+  response = {}
+  the_request = {}
+
+  if request.method == 'GET':
+    for key, value in request.args.items():
+      the_request[key] = value
+  elif request.method == 'POST':
+    for key, value in request.get_json(force=True).items():
+      the_request[key] = value
+
+  if 'url' in the_request:
+    url = the_request['url']
+    u = urlparse(url = url)
+
+    if u.scheme + '://' + u.netloc + '/' == request.host_url:
+      # If the supplied URL is the same as the app’s URL
+      # Don’t throw bricks into the machine, kids.
+      # Perhaps there should be a protection against URL-shorteners, too.
+      response['error'] = 'u kno what u did'
+
+    else:
+      # TODO : get a queue going for these kinds of jobs
+      valid_feed = feed_worker.import_feed(url)
+      
+      if not valid_feed:
+        response['error'] = 'The supplied URL is not a podcast feed.'
+        
+      else:
+
+        if 'frequency' not in the_request:
+          the_request['frequency'] = 'weekly'
+          response['warning'] = {
+            'type': 'light warning',
+            'content': 'No frequency was supplied, going with the default (weekly)'}
+
+
+        frequency = get_frequency(request_form = the_request)
+        options = get_options(request_form = the_request)
+
+        if frequency == '':
+          frequency = 'weekly'
+          response['warning'] = {
+            'type': 'stern warning',
+            'content': 'No custom day was supplied despite `custom days` frequency, going with the default frequency (weekly)'}
+
+        feed_url = url
+        if u.netloc == 'itunes.apple.com':
+          feed_url = feed_worker.get_feed_from_itunes_api(itunes_url = feed_url)
+
+        feed_object = db.session.query(Feed).filter(Feed.url == feed_url).one()
+
+        end_url = request.host_url + generate_url(feed_id = feed_object.id, frequency = frequency, options = options)
+
+
+        response['feed_id'] = feed_object.id
+        response['url'] = end_url
+
+  else:
+    response['error'] = 'No URL provided'
+        
+
+  return json.dumps(response)
