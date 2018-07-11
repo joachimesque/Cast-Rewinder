687e36bfe51c9bef193a152465166ca6daedbdfd
Joachim <joachim.robert@gmail.com>
2018-07-11 18:29:57 +0200

    Footer : add render time & git commit info

--- castrewinder/__init__.py
+++ castrewinder/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, g
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
@@ -18,3 +18,27 @@ from flask_wtf.csrf import CSRFProtect
 # csrf = CSRFProtect(app)
 
 app.secret_key = app.config['APP_SECRET_KEY']
+
+
+# Website footer info
+
+import subprocess
+import pickle
+import time
+
+@app.before_first_request
+def before_first_request():
+  # Get git ID, pickle it
+  git_id = subprocess.check_output(["git", "rev-parse","HEAD"]).decode("ascii").strip()
+  with open('pickle.pk', 'wb') as fi:
+    pickle.dump(git_id, fi)
+
+@app.before_request
+def before_request():
+  # Unpickle git ID from pickle.
+  with open('pickle.pk', 'rb') as fi:
+    g.git_id = pickle.load(fi)
+
+  # Get request time
+  g.request_start_time = time.time()
+  g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
