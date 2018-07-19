fdc0bdc116470d784ab790731c3b2817b9da53ed
Joachim <joachim.robert@gmail.com>
2018-07-19 12:32:30 +0200

    Preparing changes with *a migration*

--- castrewinder/models.py
+++ castrewinder/models.py
@@ -3,7 +3,7 @@ from . import db
 class Feed(db.Model):
     __tablename__ = 'feed'
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String, unique = True, index=True)
+    url = db.Column(db.String, unique=True, index=True)
     etag = db.Column(db.String)
     last_modified = db.Column(db.String)
     last_published_element = db.Column(db.DateTime)
@@ -19,6 +19,8 @@ class Episode(db.Model):
     published = db.Column(db.DateTime)
     content = db.Column(db.Text)
     feed_id = db.Column(db.Integer, db.ForeignKey('feed.id'), index=True)
+    enclosure_url = db.Column(db.Text)
+    enclosure_is_active = db.Column(db.Boolean, index=True)
     # Use cascade='delete,all' to propagate the deletion of a Feed onto its Employees
     feed = db.relationship('Feed',
                     backref = db.backref('episodes',
